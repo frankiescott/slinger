afde214f355fed32a991698bcb0f056ef5539f1d
Frankie <email@frankiescott.net>
2020-08-22 20:28:30 -0400

    handles /say and /yell local messages

--- Slinger.lua
+++ Slinger.lua
@@ -14,22 +14,37 @@ end
 Slinger:SetScript("OnEvent", OnEvent)
 Slinger:RegisterEvent("CHAT_MSG_CHANNEL")
 Slinger:RegisterEvent("ADDON_LOADED")
-
+Slinger:RegisterEvent("CHAT_MSG_SAY")
+Slinger:RegisterEvent("CHAT_MSG_YELL")
 function Slinger:ADDON_LOADED(addonName)
     active = false
 end
 
 function Slinger:CHAT_MSG_CHANNEL(msg, author, language, channel, ...)
     message = string.lower(msg)
-    --if channel == "2. Trade - City" then
-        --print("A user messaged trade")
-    --end
+
     if channel == "1. General - Stormwind City" then
-        for i, phrase in ipairs(phrases) do
-            if string.match(message, phrase) then
-                SendChatMessage("I can port you!", "WHISPER", "Common", author);
-                InviteUnit(author)
-            end
+        parseLocal(message, author)
+    end
+
+    if channel == "5. FrankupTest" then
+        parseLocal(message, author)
+    end
+end
+
+function Slinger:CHAT_MSG_SAY(msg, author, ...)
+    parseLocal(msg, author)
+end
+
+function Slinger:CHAT_MSG_YELL(msg, author, ...)
+    parseLocal(msg, author)
+end
+
+function parseLocal(msg, author)
+    for i, phrase in ipairs(phrases) do
+        if string.match(message, phrase) then
+            SendChatMessage("I can port you!", "WHISPER", "Common", author);
+            InviteUnit(author)
         end
     end
 end
